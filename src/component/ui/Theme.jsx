2dca71eab7e875f624d954ebf0dc8a089e6defb3
Michael <kllmmc23@gmail.com>
2020-06-11 22:28:38 -0600

    Styling themes typography

--- src/component/ui/Theme.jsx
+++ src/component/ui/Theme.jsx
@@ -1,19 +1,24 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-const arcBlack = '#1C1C1C';
-const arcRed = '#D90B0B';
+const arcBlack = "#1C1C1C";
+const arcRed = "#D90B0B";
 
 export default createMuiTheme({
-    palette: {
-        common: {
-            blue: `${arcBlack}`,
-            orange: `${arcRed}`
-        },
-        primary: {
-            main: `${arcBlack}`
-        },
-        secondary: {
-            main: `${arcRed}`
-        }
-    }
+  palette: {
+    common: {
+      blue: `${arcBlack}`,
+      orange: `${arcRed}`,
+    },
+    primary: {
+      main: `${arcBlack}`,
+    },
+    secondary: {
+      main: `${arcRed}`,
+    },
+  },
+  typography: {
+    h3: {
+      fontWeight: "100",
+    },
+  },
 });
